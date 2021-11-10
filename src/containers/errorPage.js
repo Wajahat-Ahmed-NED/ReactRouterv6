f0bc8922c2a06eac5110e9b208e293d9225e4def
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-11-10 22:09:58 +0500

    Practicing

---
+++ src/containers/errorPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ErrorPage() {
+    return (
+        <div>
+            <h1>404 Error</h1>
+            <h2>Page Not Found</h2>
+        </div>
+    )
+}
